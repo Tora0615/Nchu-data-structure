3c8d97a4bfec51a8bf8dabf2797933e2a0d45f0e
Tora0615 <5438arthur@gmail.com>
2021-11-14 06:15:32 +0800

    feat : add function of delete node form index.  style : change variable "tailIndex" to "nodeCount".

--- homework/homework02/4106030323_hw2-1.c
+++ homework/homework02/4106030323_hw2-1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #define TRUE 1
 #define FALSE 0
 
@@ -16,12 +17,18 @@ typedef struct listNode {
 FILE *f_write_ptr;
 const char *filename_output = "HW1_output_1-1.txt";
 listPointer first = NULL;
-int tailIndex = 0;
+int nodeCount = 0;
 void initLinkedList(char *);
 void addToEnd(int);
 void decodeOp(char *);
 void showLinkedList(void);
-//int getOpCount();
+int getOpCount(char *);
+void decodeOp(char *);
+int stringToInt(char *contents);
+void delFromIndex(int);
+void insertAfterIndex(int, int);
+void multipleFromReverseIndex(int, int);
+void reverseGroupOfXItems(int);
 
 
 int main(int argc, char const *argv[]) {
@@ -62,24 +69,27 @@ int main(int argc, char const *argv[]) {
   endLineBuf = fgetc(f_read_ptr); //吃掉換行符號
   contents[0] = '\0'; // 歸零 contents
 
-  showLinkedList();
-  exit(EXIT_FAILURE);
+  //showLinkedList();
+
 
   // 讀入有幾筆操作
   fscanf(f_read_ptr,"%[^\n]",contents);  //讀到換行前
   int opCount = 0;
-  //opCount = getOpCount(contents);
+  opCount = getOpCount(contents);
   endLineBuf = fgetc(f_read_ptr); //吃掉換行符號
   contents[0] = '\0'; // 歸零 contents
 
+
+
   int i;
   for(i=0;i<opCount;i++){
-
     fscanf(f_read_ptr,"%[^\n]",contents);  //讀到換行前
     endLineBuf = fgetc(f_read_ptr); //吃掉換行符號
-
-
+    decodeOp(contents);
   }
+  showLinkedList();
+  printf("%d\n",nodeCount);
+  exit(EXIT_FAILURE);
 
   fclose(f_read_ptr);
   fclose(f_write_ptr);
@@ -98,35 +108,46 @@ void addToEnd(int data){
 
   if (!first){ // if empty
     first = temp;
-    tailIndex += 1;
+    nodeCount += 1;
   }else{ // not empty
     int i;
     listPointer tail = first;
-    for(i=0;i<tailIndex-1;i++){
+    for(i=0;i<nodeCount-1;i++){
       tail = tail->link;
     }
     tail->link = temp;
-    tailIndex += 1;
+    nodeCount += 1;
   }
 }
 
 // 刪除第n個
-void delFromIndex(){
-  // if not empty
+void delFromIndex(int index){
+  listPointer now = NULL, next = NULL;
+  // get now and next mem position
+  now = first+index-1;
+  next = now->link;
+  // now.data = next.data
+  now->data = next->data;
+  // now.link = next.link
+  now->link = next->link;
+  // free next
+  free(next);
+
+  nodeCount -= 1;  // 要加不然會 segment fault
 }
 
 // 插入到第n個後
-void insertAfterIndex(){
+void insertAfterIndex(int index, int data){
 
 }
 
 // 後面數過來第幾個元素乘與幾倍
-void multipleFromReverseIndex(){
+void multipleFromReverseIndex(int r_count, int mult){
 
 }
 
 // 幾個一組 & 反轉
-void reverseGroupOfXItems(){
+void reverseGroupOfXItems(int groupCount){
 
 }
 
@@ -161,25 +182,44 @@ void initLinkedList(char *contents){
   }
 }
 
+int getOpCount(char *contents){
+  return stringToInt(contents);
+}
+
+int stringToInt(char *contents){
+  int index = 0, count = 0;
+  while(*(contents + index) != '\0' && *(contents + index) != '\r' && *(contents + index) != ' '){
+    count = count*10 + (*(contents + index) - '0');
+    index += 1;
+  }
+  // printf("%d\n",count);
+  return count;
+}
+
 //
 void decodeOp(char *contents){
-  char opcode[6];
-  int data = -1;
-  char charBuf = ' ';
-  int readIndex = -1;
-  // while (TRUE){
-  //   readIndex += 1;
-  //   charBuf = contents[readIndex];
-  //   if(charBuf == ' ' ){
-  //     addToEnd(tempNum);
-  //     tempNum = 0;
-  //     continue;
-  //   }else if(charBuf == '\0' || charBuf == '\r'){
-  //     addToEnd(tempNum);
-  //     tempNum = 0;
-  //     break;
-  //   }else{
-  //     tempNum = tempNum * 10 + (charBuf - '0');
-  //   }
-  // }
+  int startIndex = -1;  // 數字起始位置
+
+  while (!isspace(contents[startIndex++]));
+
+  switch (contents[0]) {
+    case 'a':  //add
+      addToEnd(stringToInt(&contents[startIndex]));
+      break;
+    case 'd':  //del
+      delFromIndex(stringToInt(&contents[startIndex]));
+      break;
+    case 'r':  //rev
+      //reverseGroupOfXItems();
+      break;
+    case 's':  //show
+      //showLinkedList();
+      break;
+    case 'i':  //ins
+      //insertAfterIndex();
+      break;
+    case 'm':  //mul
+      //multipleFromReverseIndex();
+      break;
+  }
 }
