36e964fd1949d6c6f5f5211e91420db4f5783c23
Tora0615 <5438arthur@gmail.com>
2021-11-19 11:45:20 +0800

    feat : complete the original part of read file

--- homework/homework02/4106030323_hw2-2.c
+++ homework/homework02/4106030323_hw2-2.c
@@ -3,6 +3,9 @@
 #include <ctype.h>
 #define TRUE 1
 #define FALSE 0
+#define orderPATH "../testData/test_case_2-1/orders.txt"
+#define recipePATH "../testData/test_case_2-1/recipes.txt"
+
 
 FILE *f_write_ptr;
 const char *filename_output = "output_2.txt";
@@ -26,25 +29,40 @@ typedef struct _componentnode {
   componentpointer next;
 } componentnode;
 
-typedef struct _recipiesnode *recipiespointer;
-typedef struct _recipiesnode {
+typedef struct _recipesnode *recipespointer;
+typedef struct _recipesnode {
   char* recipe_name;
   componentpointer stove;
   componentpointer cut;
   componentpointer others;
-  recipiespointer pre;
-  recipiespointer next;
-} recipiesnode;
+  recipespointer pre;
+  recipespointer next;
+} recipesnode;
+
+
+void readOrders(FILE*, orderpointer);
+void readRecipe(FILE*, recipespointer);
 
 int main(int argc, char const *argv[]) {
   FILE *f_read_ptr;
-  char *filename_input = "../testData/test_case_2-1/orders.txt"; // 要注意 input file path
+  char *filename_input = orderPATH; // define in macro
+  orderpointer orderHead = NULL;
+  recipespointer recipeHead = NULL;
 
-  // input file 01 open
+  // input file order open
   if (!(f_read_ptr = fopen(filename_input,"r"))){
     printf("Cannot open file : %s\n", filename_input);
     exit(EXIT_FAILURE);
   }
+  readOrders(f_read_ptr, orderHead);
+
+  filename_input = recipePATH; // 要注意 input file path
+  // input file recipe open
+  if (!(f_read_ptr = fopen(filename_input,"r"))){
+    printf("Cannot open file : %s\n", filename_input);
+    exit(EXIT_FAILURE);
+  }
+  readRecipe(f_read_ptr, recipeHead);
 
   // open output file with fool proof.
   if((f_write_ptr = fopen(filename_output,"r"))){
@@ -63,4 +81,33 @@ int main(int argc, char const *argv[]) {
     f_write_ptr = fopen(filename_output,"w");
   }*/
   return 0;
+}
+
+
+void readOrders(FILE *f_write_ptr, orderpointer orderHead){
+  char buf;
+  char context[100];
+  fscanf(f_write_ptr,"%[^\n]",context);
+  buf = fgetc(f_write_ptr);
+
+  int i = 0, range = 0;
+  while(context[i] != '\0' && context[i] != '\r'){
+    range = range * 10 + context[i] - '0' ;
+    i+=1;
+  }
+  printf("count : %d\n",range);
+}
+
+void readRecipe(FILE *f_write_ptr, recipespointer recipeHead){
+  char buf;
+  char context[100];
+  fscanf(f_write_ptr,"%[^\n]",context);
+  buf = fgetc(f_write_ptr);
+
+  int i = 0, range = 0;
+  while(context[i] != '\0' && context[i] != '\r'){
+    range = range * 10 + context[i] - '0' ;
+    i+=1;
+  }
+  printf("count : %d\n",range);
 }
