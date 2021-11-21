73cf3e2e37e1b531d08965d57c92ededb2df4741
Tora0615 <5438arthur@gmail.com>
2021-11-21 15:22:14 +0800

    feat : complete the function of add recipes (can add components to the nested structure)
    1. dump the data in the structure is also succed.

--- homework/homework02/4106030323_hw2-2.c
+++ homework/homework02/4106030323_hw2-2.c
@@ -42,8 +42,10 @@ typedef struct _recipesnode {
 
 
 orderpointer readOrders(FILE*, orderpointer);
-void readRecipe(FILE*, recipespointer);
+recipespointer readRecipe(FILE*, recipespointer);
 int getValue(char*);
+componentpointer addcomponent(componentpointer, char*);
+void dumpRecipes(recipespointer);
 
 int main(int argc, char const *argv[]) {
   FILE *f_read_ptr;
@@ -57,7 +59,7 @@ int main(int argc, char const *argv[]) {
     exit(EXIT_FAILURE);
   }
   orderHead = readOrders(f_read_ptr, orderHead);  //傳指標進去是複製一份....，要再傳出來
-
+  //printf("%s\n",orderHead->next->recipe_name);
 
   filename_input = recipePATH; // 要注意 input file path
   // input file recipe open
@@ -65,7 +67,9 @@ int main(int argc, char const *argv[]) {
     printf("Cannot open file : %s\n", filename_input);
     exit(EXIT_FAILURE);
   }
-  readRecipe(f_read_ptr, recipeHead);
+  recipeHead = readRecipe(f_read_ptr, recipeHead);
+  dumpRecipes(recipeHead);
+
 
   // open output file with fool proof.
   if((f_write_ptr = fopen(filename_output,"r"))){
@@ -104,9 +108,11 @@ orderpointer readOrders(FILE *f_write_ptr, orderpointer orderHead){
 
   // save data to linked list
   for(i = 0; i<range ; i++){  //幾筆資料
+
+    char* tempc = malloc(sizeof(char)*50);
+
     // 宣告節點
     orderpointer temp = malloc(sizeof(ordernode));
-    char* tempc = malloc(sizeof(char)*50);
 
     // 節點初始化
     temp -> order_ID = -1;
@@ -122,7 +128,7 @@ orderpointer readOrders(FILE *f_write_ptr, orderpointer orderHead){
 
     int j;
     for (j = 0; j < 6; j++) {
-      fscanf(f_write_ptr,"%[^ &&[^\n]]",context);
+      fscanf(f_write_ptr,"%[^ &&[^\n]]",context); //不等於空格(^ )以及不等於換行符號(^\n)
       buf = fgetc(f_write_ptr);
 
       //change here
@@ -171,11 +177,12 @@ orderpointer readOrders(FILE *f_write_ptr, orderpointer orderHead){
   return orderHead;
 }
 
-void readRecipe(FILE *f_write_ptr, recipespointer recipeHead){
+recipespointer readRecipe(FILE *f_write_ptr, recipespointer recipeHead){
   char buf;
   char context[100];
   fscanf(f_write_ptr,"%[^\n]",context);
   buf = fgetc(f_write_ptr);
+  recipespointer pre = NULL;
 
   // get data count
   int i = 0, range = 0;
@@ -186,36 +193,62 @@ void readRecipe(FILE *f_write_ptr, recipespointer recipeHead){
   //printf("count : %d\n",range);
 
   // save data to linked list
-  for(i = 0; i<range ; i++){
+  for(i = 0; i<range ; i++){  //幾筆資料
+
+
+
+    // 宣告節點
+    recipespointer temp = malloc(sizeof(recipesnode));
+
+    // 節點初始化
+    temp -> recipe_name = malloc(sizeof(char)*50);
+    temp -> recipe_name[0] = '\0';
+    temp -> stove = NULL;
+    temp -> cut = NULL;
+    temp -> others = NULL;
+    temp -> pre = NULL;
+    temp -> next = NULL;
+
     int j;
-    for (j = 0; j < 4; j++) {
-      // fscanf(f_write_ptr,"%[^ ]",context);
+    for (j = 0; j < 4; j++) {  //一筆資料中有四個區塊
+
+      // 讀一個區塊
       fscanf(f_write_ptr,"%[^ &&[^\n]]",context);  //不等於空格(^ )以及不等於換行符號(^\n)
       buf = fgetc(f_write_ptr);
-      //printf("%s\n",context);
 
       int k = 0;
       char *tempStr = malloc(sizeof(char)*100);
       int startIndex = 0;
       tempStr[0] = '\0';
+
       while(context[k] != '\r' && context[k] != '\n' && context[k] != '\0'){
+        //逗號前
         if(context[k] == ','){
+          char* tempc = malloc(sizeof(char)*50);
           tempStr = &context[startIndex];
           tempStr[k] = '\0';
 
           //change here
           switch (j) {
             case 0:
-              printf("recipe_name : %s\n",tempStr);
+              //printf("recipe_name : %s\n",tempStr);
+              strcpy(tempc, tempStr);
+              temp -> recipe_name = tempc;
               break;
             case 1:
-              printf("stove : %s\n",tempStr);
+              // printf("stove : %s\n",tempStr);
+              strcpy(tempc, tempStr);
+              temp->stove = addcomponent(temp->stove, tempc);
               break;
             case 2:
-              printf("cut : %s\n",tempStr);
+              // printf("cut : %s\n",tempStr);
+              strcpy(tempc, tempStr);
+              temp->cut = addcomponent(temp->cut, tempc);
               break;
             case 3:
-              printf("others : %s\n",tempStr);
+              // printf("others : %s\n",tempStr);
+              strcpy(tempc, tempStr);
+              temp->others = addcomponent(temp->others, tempc);
               break;
           }
 
@@ -228,24 +261,42 @@ void readRecipe(FILE *f_write_ptr, recipespointer recipeHead){
       tempStr = &context[startIndex];
       tempStr[k] = '\0';
 
-      //change here
+      //逗號後
+      char* tempc = malloc(sizeof(char)*50);
       switch (j) {
         case 0:
-          printf("recipe_name : %s\n",tempStr);
+          //printf("recipe_name : %s\n",tempStr);
+          strcpy(tempc, tempStr);
+          temp -> recipe_name = tempc;
           break;
         case 1:
-          printf("stove : %s\n",tempStr);
+          //printf("stove : %s\n",tempStr);
+          strcpy(tempc, tempStr);
+          temp->stove = addcomponent(temp->stove, tempc);
           break;
         case 2:
-          printf("cut : %s\n",tempStr);
+          //printf("cut : %s\n",tempStr);
+          strcpy(tempc, tempStr);
+          temp->cut = addcomponent(temp->cut, tempc);
           break;
         case 3:
-          printf("others : %s\n",tempStr);
+          //printf("others : %s\n",tempStr);
+          strcpy(tempc, tempStr);
+          temp->others = addcomponent(temp->others, tempc);
           break;
       }
-
+    }
+    // 接 node
+    if(!recipeHead){
+      recipeHead = temp;
+      pre = temp;
+    }else{
+      temp->pre = pre;
+      pre->next = temp;
+      pre = temp;
     }
   }
+  return recipeHead;
 }
 
 
@@ -256,6 +307,51 @@ int getValue(char *str){
     k+=1;
   }
   return num;
+}
+
+componentpointer addcomponent(componentpointer operHead, char* context){
+
+  // 找到 pre node
+  componentpointer pre = operHead;
+  while(pre && pre->next){
+    pre = pre->next;
+  }
+
+  // 初始化
+  componentpointer temp = malloc(sizeof(componentnode));
+  temp -> name = malloc(sizeof(char)*50);
+  temp -> name[0] = '\0';
+
+  // 給值
+  temp -> name = context;
+
+  // 接link
+  if(!operHead){
+    operHead = temp;
+  }else{
+    temp->pre = pre;
+    pre->next = temp;
+  }
+  return operHead;
+}
+
+void dumpRecipes(recipespointer recipeHead){
+  while(recipeHead){
+    printf("%s\n",recipeHead->recipe_name);
+    while(recipeHead->stove){
+      printf("%s\n",recipeHead->stove->name);
+      recipeHead->stove = recipeHead->stove->next;
+    }
+    while(recipeHead->cut){
+      printf("%s\n",recipeHead->cut->name);
+      recipeHead->cut = recipeHead->cut->next;
+    }
+    while(recipeHead->others){
+      printf("%s\n",recipeHead->others->name);
+      recipeHead->others = recipeHead->others->next;
+    }
+    recipeHead = recipeHead->next;
+  }
 }
 
 
