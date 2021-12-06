bb5319f099ddb9a5cd645b72f25ea2bc26c9c9ac
Tora0615 <5438arthur@gmail.com>
2021-12-06 14:43:37 +0800

    feat : add API interface

--- homework/homework03/4106030323_hw3-1.c
+++ homework/homework03/4106030323_hw3-1.c
@@ -10,14 +10,81 @@
 FILE *f_read_ptr;
 FILE *f_write_ptr;
 
+typedef struct _node *nodePointer;
+typedef struct _node{
+	int data;
+	nodePointer left;
+	nodePointer right;
+} node;
+
+
+// insert node x
+void insertNode(nodePointer root, int data){
+
+	nodePointer temp = (nodePointer)malloc(sizeof(node));
+	temp->data = data;
+
+	// root == NULL
+	if(!root){
+
+	}
+	// check node exist
+	if(1){
+		return;
+	}else{
+		// add node
+	}
+}
+
+
+// del node x
+void deleteNode(nodePointer root, int data){
+
+	if(1){				// check node exist
+		return;
+	}else{				// del node
+		if(1){			// Leaf (next)
+
+		}else if(1){	//	if has left tree
+
+		}else{			//
+
+		}
+	}
+}
+
+
+// query node x, get deepth
+void queryNode(nodePointer root, int data){
+	// check node exist
+	if(1){
+		return;
+	}else{
+		// query node
+	}
+}
+
+
+// print form node x to y
+void printSum(nodePointer root, int pointA, int pointB){
+	// check node exist
+	if(1 && 1){
+		return;
+	}else{
+		// printSum
+	}
+}
+
+
+
 
 int main(int argc, char const *argv[]) {
-	
+
 	if (!(f_read_ptr = fopen(INPUTFILE,"r"))){
 		printf("Cannot open file : %s\n", INPUTFILE);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	if((f_write_ptr = fopen(OUTPUTFILE,"r"))){
 		fclose(f_write_ptr);
 		printf("File %s have already exist.\nContinue to overwrite output file ? [Y/N] ",OUTPUTFILE);
@@ -32,19 +99,19 @@ int main(int argc, char const *argv[]) {
 		}
 	} /*else {
 		f_write_ptr = fopen(OUTPUTFILE,"w");
-	}*/ 
-	
+	}*/
+
 	char endLineBuf = ' ';
 	char contents[10000];
-	
-	
+
+
 	while(TRUE){
-		fscanf(f_read_ptr,"%[^\n]",contents); 
+		fscanf(f_read_ptr,"%[^\n]",contents);
 		endLineBuf = fgetc(f_read_ptr);
 		int dataCount = -1;
 		int opCount = -1;
 		char charBuf = ' ';
-		
+
 		// get data count (M)
 		int tempNum = 0, readIndex = -1;
 		while (TRUE){
@@ -58,8 +125,8 @@ int main(int argc, char const *argv[]) {
 		    tempNum = tempNum * 10 + (charBuf - '0');
 		  }
 		}
-		
-		
+
+
 		// get op count (N)
 		while (TRUE){
 		  readIndex += 1;
@@ -73,21 +140,21 @@ int main(int argc, char const *argv[]) {
 		    tempNum = tempNum * 10 + (charBuf - '0');
 		  }
 		}
-		
+
 		//printf("%d %d\n", dataCount, opCount);
-		
+
 		if( dataCount == 0 && opCount == 0){
 			break;
 		}else{
 			/* read data (M datas)*/
-			fscanf(f_read_ptr,"%[^\n]",contents); 
+			fscanf(f_read_ptr,"%[^\n]",contents);
 			endLineBuf = fgetc(f_read_ptr);
 			printf("%s\n",contents);
-			
+
 			/* process opers */
 			int i;
 			for( i = 0 ; i < opCount ; i++){
-				fscanf(f_read_ptr,"%[^\n]",contents); 
+				fscanf(f_read_ptr,"%[^\n]",contents);
 				endLineBuf = fgetc(f_read_ptr);
 				printf("%s\n",contents);
 			}
@@ -95,8 +162,8 @@ int main(int argc, char const *argv[]) {
 		dataCount = -1;
 		opCount = -1;
 	}
-	
-	
+
+
 	fclose(f_read_ptr);
 	fclose(f_write_ptr);
 	printf("Complete !\n");
