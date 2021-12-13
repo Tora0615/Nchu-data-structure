c01b9c0e8502cd2ea683d92b52bc959a1993bff9
Tora0615 <5438arthur@gmail.com>
2021-12-13 17:40:19 +0800

    feat : done paint wall

--- homework/homework03/4106030323_hw3-3.c
+++ homework/homework03/4106030323_hw3-3.c
@@ -10,6 +10,15 @@
 FILE *f_read_ptr;
 FILE *f_write_ptr;
 
+typedef struct _node *nodePointer;
+typedef struct _node{
+	int start;
+	int end;
+	nodePointer left;
+	nodePointer right;
+	char color;
+} node ;
+
 int stringToInt(char *contents){
   	int index = 0, count = 0, isNegative = FALSE;
   	while(*(contents + index) != '\0' && *(contents + index) != '\r' && *(contents + index) != ' '){
@@ -26,11 +35,66 @@ int stringToInt(char *contents){
 	return count;
 }
 
-void paintWall(int start, int end, char color){
+void paintWall(nodePointer root, int start, int end, char color){
 	// printf("P : %d %d %c\n",start, end, color);
+	
+	// if paint range equal root node range
+	if(root->start == start && root->end == end){
+		root->color = color;
+		root->left = NULL;
+		root->right = NULL;
+		// free tree
+	}else{
+		int range = (root->end - root->start) / 2 ;
+		// creat left child (half range)
+		if(!root->left){
+			nodePointer temp = (nodePointer)malloc(sizeof(node));
+			temp->left = NULL;
+			temp->right = NULL;
+			temp->start = root->start;
+			temp->end = root->start + range;
+			temp->color = root->color;
+			root->left = temp;
+		}
+		
+		// creat right child (half range)
+		if(!root->right){
+			nodePointer temp = (nodePointer)malloc(sizeof(node));
+			temp->left = NULL;
+			temp->right = NULL;
+			temp->start = root->start + range + 1;
+			temp->end = root->end;
+			temp->color = root->color;
+			root->right = temp;
+		}
+		
+		
+		// recursive process two child (set them as new root)
+		if(mid > start && mid < end || mid+1 > start && mid+1 <end || mid == start && mid+1 ==end){
+			paintWall(node->left, start, end, color);
+			paintWall(node->right, start, end, color);
+			node->color = '?';
+			return;
+		}
+		// if need range in left child  or current node's start in need range
+		else if( (root->left->start <= start && root->left->end >= end)  ||  (root->start >= start && root->start <= end) ){
+			// use left node as root and do again
+			paintWall(node->left, start, end, color);
+			node->color = '?';
+			return;
+		}
+		
+		// if need range in right child or current node's end in need range
+		else if( (root->right->start <= start && root->right->end >= end)  ||  (root->end >= start && root->end <= end) ){
+			// use right node as root and do again
+			paintWall(node->right, start, end, color);
+			node->color = '?';
+			return;
+		}	
+	}
 }
 
-void queryRange(int start, int end){
+void queryRange(nodePointer root, int start, int end){
 	// printf("Q : %d %d\n",start, end);
 }
 
@@ -65,6 +129,14 @@ int main(int argc, char const *argv[]) {
 	int wallLen = stringToInt(contents);
 	endLineBuf = fgetc(f_read_ptr);
 	
+	nodePointer treeRoot;
+	treeRoot = (nodePointer)malloc(sizeof(node));
+	treeRoot->start = 0;
+	treeRoot->end = wallLen;
+	treeRoot->left = NULL;
+	treeRoot->right = NULL;
+	treeRoot->color = '_';
+	
 	fscanf(f_read_ptr,"%[^\n]",contents);
 	int dataCount = stringToInt(contents);
 	endLineBuf = fgetc(f_read_ptr);
@@ -85,9 +157,9 @@ int main(int argc, char const *argv[]) {
 		if(contents[0] == 'P'){
 			while(!isspace(contents[index++]));
 			char color = contents[index];
-			paintWall(start, end, color);
+			paintWall(treeRoot, start, end, color);
 		}else{
-			queryRange(start, end);
+			queryRange(treeRoot, start, end);
 		}
 	}
 
