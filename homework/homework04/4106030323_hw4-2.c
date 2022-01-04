7e182573ac938017c220fa921b7d01de8476dff7
Tora0615 <5438arthur@gmail.com>
2022-01-04 22:03:17 +0800

    feat : Test 4-2 but havent yet.

--- homework/homework04/4106030323_hw4-2.c
+++ homework/homework04/4106030323_hw4-2.c
@@ -15,6 +15,23 @@
 FILE *f_read_ptr;
 FILE *f_write_ptr;
 
+typedef struct _node *nodepointer;
+typedef struct _node{
+	int data;
+	nodepointer next;
+	int cost;  // to this node cost how muxh
+}node;
+
+typedef struct _header{
+	nodepointer start;
+	//int visited;
+}header;
+
+
+void DFS(int *visitedArr, ){
+	
+}
+
 int stringToInt(char *contents){
   	int index = 0, count = 0, isNegative = FALSE;
   	while(*(contents + index) != '\0' && *(contents + index) != '\r' && *(contents + index) != ' '){
@@ -62,7 +79,55 @@ int main(int argc, char const *argv[]) {
 	
 	fscanf(f_read_ptr,"%[^\n]",contents);
 	endLineBuf = fgetc(f_read_ptr);
+	int index = 0;
+	int maxNodes = stringToInt(contents);
+	while(!isspace(contents[index++]));
+	int orders = stringToInt(&contents[index]);
+//	printf("%d %d",maxNodes,orders);
 
+	// init header
+	header headerList[maxNodes];
+	int i;
+//	for(i=0 ; i<maxNodes ; i++){
+//		headerList[i].visited = 0;
+//	}
+	
+	
+	for(i=0 ; i<maxNodes-1 ; i++){
+		int index = 0;
+		int u = stringToInt(contents);
+		while(!isspace(contents[index++]));
+		int v = stringToInt(&contents[index]);
+		while(!isspace(contents[index++]));
+		int p = stringToInt(&contents[index]);
+		nodepointer tempA = (nodepointer)malloc(sizeof(node));
+		tempA->data = v;
+		tempA->next = NULL;
+		tempA->cost = p;
+		if(!headerList[u-1].start){
+			headerList[u-1].start = tempA;
+		}else{
+			nodepointer head = headerList[u-1].start;
+			while(head->next){
+				head = head->next; 
+			}
+			head->next = tempA;
+		}
+		
+		nodepointer tempB = (nodepointer)malloc(sizeof(node));
+		tempB->data = u;
+		tempB->next = NULL;
+		tempB->cost = p;
+		if(!headerList[u-1].start){
+			headerList[u-1].start = tempB;
+		}else{
+			nodepointer head = headerList[u-1].start;
+			while(head->next){
+				head = head->next; 
+			}
+			head->next = tempB;
+		}
+	}
 
 	fclose(f_read_ptr);
 	fclose(f_write_ptr);
